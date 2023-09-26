4edfa37d0df1def6099940f95a1bf5dea9d68d25
Alex <alexpop@metropolia.fi>
2023-09-26 15:20:32 +0300

    Project

--- main.py
+++ main.py
@@ -1,36 +1,27 @@
-1.
-user = input('Enter your name: ')
-print('Hello, ' + user +"!")
+import mysql
+import pygame
+connection = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='',
+         autocommit=True
+         )
+def LocalAirportFetcher():
+    return True
+def InternationalAirportFetcher():
+    #for simplicity sake, all large airports are treated as large
+    return True
+# After the algorithm fetches the airports, the player is asked if they want to select to go to one of the airports.
+# If the player says no they get returned to the main menu, otherwise the flight game starts
+def FlightGame():
+    return True
 
-2.
-radius = int(input("Enter the radius of a circle: "))
-circle_area = radius * radius * 3.14
-print("Area of the circle is: " , circle_area)
-
-3.
-width = int(input("Enter the length of the rectangle: "))
-height = int(input("Enter the height of the rectangle: "))
-perimeter = width*2+height*2
-area = width* height
-print("Perimeter of rectangle is: ", perimeter)
-print("area of rectangle is: ", area)
-
-4.
-n1=int(input("Insert number 1: "))
-n2=int(input("Insert number 2: "))
-n3=int(input("Insert number 3: "))
-print("Sum of the numbers is:", n1+n2+n3)
-print("Product of the numbers is:", n1*n2*n3)
-print("Average of the numbers is:", (n1+n2+n3)/3)
-
-5.
-import math
-talents = float(input("Enter talents: "))
-pounds = float(input("Enter pounds: "))
-lots = float(input("Enter lots: "))
-mweight = talents*20*32*13.3+pounds*32*13.3+lots*13.3
-mgram = mweight%1000
-print("The weight in modern units is:/n")
-print(int(mweight/1000),f" kilograms and {mgram:.3f}")
-
-6.
+run = True
+while run == True:
+    #The program will have to remind the player what airport they are located in:
+    #Or if they crashed
+    Print("1 - What local airports are available?")
+    Print("2 - What international airports are available?")
+    UsInput = int(input("Which choice would you like to pick?"))
