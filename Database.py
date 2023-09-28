1df9e401652bd39bc66bc57d0b12614687cd8ba8
ashifaaa <ashifahossain10@gmail.com>
2023-09-29 01:31:59 +0300

    Added the functionality for the player to move. Also, refactored so that the database file only has database concerns.

--- Database.py
+++ Database.py
@@ -1,5 +1,4 @@
-import mysql.connector
-
+# All MariaDB interactions go here
 
 # takes sql text for query and cursor and returns result of query
 def db_query(sql, cursor):
@@ -11,6 +10,19 @@ def db_query(sql, cursor):
 def display_list(print_list):
     for x in print_list:
         print(x)
+
+
+# sends user location to the db
+def set_user_location(location, id, cursor):
+    cursor.execute(f"update game set location = '{location}' where id = '{id}'")
+
+
+# gets user location from the db
+def get_user_location(id, cursor):
+    return db_query(f"select game.location, airport.name "
+                    f"from game, airport "
+                    f"where game.id= '{id}'"
+                    f"and game.location = airport.ident", cursor)[0]
 
 
 # takes continent code as a string and returns the full name of the continent
@@ -42,57 +54,9 @@ def get_airport_type_list(cursor, country):
 
 
 def get_airport_list(cursor, country, airport_type):
-    return db_query(f"select airport.name "
+    return db_query(f"select airport.name, airport.ident "
                     f"from airport, country "
                     f"where airport.iso_country = country.iso_country "
                     f"and country.name = '{country}' "
                     f"and airport.type = '{airport_type}'", cursor)
-
-
-# Takes a list and displays as numbered menu
-def display_menu_list(disp_list):
-    counter = 1
-    for x in disp_list:
-        print(f"{counter}. {x[0]}")
-        counter += 1
-
-
-def display_continent_list(continents):
-    counter = 1
-    for x in continents:
-        print(f"{counter}. {get_continent(x[0])}")
-        counter += 1
-
-
-def cli_get_airport_type_from_user(cursor):
-
-    # Get continent from user
-    display_continent_list(get_continent_list(cursor))
-    selection = int(input("Select Continent: "))
-
-    continents = get_continent_list(cursor)
-    continent_sel = continents[selection - 1][0]
-
-    # Get country from user
-    display_menu_list(get_country_list(cursor, continent_sel))
-    selection = int(input("Select Country: "))
-
-    countries = get_country_list(cursor, continent_sel)
-    country_sel = countries[selection - 1][0]
-
-    # Get airport_type from user
-    display_menu_list(get_airport_type_list(cursor, country_sel))
-    selection = int(input("Select Airport Type: "))
-
-    airport_types = get_airport_type_list(cursor, country_sel)
-    airport_type_sel = airport_types[selection - 1][0]
-
-    # Display available airports
-    display_menu_list(get_airport_list(cursor, country_sel, airport_type_sel))
-    selection = int(input("Select Airport: "))
-
-    airports = get_airport_list(cursor, country_sel, airport_type_sel)
-    airport_sel = airports[selection - 1][0]
-
-    print(f"\nUser has selected {airport_sel}")
 
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import mysql
+import mysql.connector
 import pygame
-from InternationalAirportSelection import cli_get_airport_type_from_user
+from Database import (get_continent, get_continent_list, get_airport_list,
+                      get_airport_type_list, get_country_list, get_user_location, set_user_location)
 
 connection = mysql.connector.connect(
          host='127.0.0.1',
          port= 3306,
          database='flight_game',
-         user='root',
-         password='',
+         user='flight_sim',
+         password='menudb',
          autocommit=True
          )
 
@@ -17,9 +18,56 @@ cursor = connection.cursor()
 def LocalAirportFetcher():
     return True
 
-def InternationalAirportFetcher(cursor):
+# Takes a list and displays as numbered menu
+def display_menu_list(disp_list):
+    counter = 1
+    for x in disp_list:
+        print(f"{counter}. {x[0]}")
+        counter += 1
+
+def display_continent_list(continents):
+    counter = 1
+    for x in continents:
+        print(f"{counter}. {get_continent(x[0])}")
+        counter += 1
+
+
+def InternationalAirportFetcher(cursor, user_id=1):
     # This function will run a cli menu where the user selects an international airport
-    cli_get_airport_type_from_user(cursor)
+    location = get_user_location(user_id, cursor)
+    print(f"Current location: {location[1]}")
+
+    # Get continent from user
+    display_continent_list(get_continent_list(cursor))
+    selection = int(input("Select Continent: "))
+
+    continents = get_continent_list(cursor)
+    continent_sel = continents[selection - 1][0]
+
+    # Get country from user
+    display_menu_list(get_country_list(cursor, continent_sel))
+    selection = int(input("Select Country: "))
+
+    countries = get_country_list(cursor, continent_sel)
+    country_sel = countries[selection - 1][0]
+
+    # Get airport_type from user
+    display_menu_list(get_airport_type_list(cursor, country_sel))
+    selection = int(input("Select Airport Type: "))
+
+    airport_types = get_airport_type_list(cursor, country_sel)
+    airport_type_sel = airport_types[selection - 1][0]
+
+    # Display available airports
+    display_menu_list(get_airport_list(cursor, country_sel, airport_type_sel))
+    selection = int(input("Select Airport: "))
+
+    airports = get_airport_list(cursor, country_sel, airport_type_sel)
+    airport_sel = airports[selection - 1][0]
+
+    set_user_location(airports[selection - 1][1], user_id, cursor)
+
+    print(f"\nLocation updated to {airport_sel}")
 
 # After the algorithm fetches the airports, the player is asked if they want to select to go to one of the airports.
 # If the player says no they get returned to the main menu, otherwise the flight game starts
@@ -28,7 +76,7 @@ def FlightGame():
 
 
 class Player:
-    location
+    location = None
     CO2_Budget = 10000
     Fuel = 100
     Money = 100
