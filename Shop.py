aa17b5545eebedc2c7f0781d8dc455aa9b95dc5b
bytecodeble <yyilin@yahoo.com>
2023-10-01 10:31:03 +0300

    test

---
+++ Shop.py
@@ -0,0 +1,35 @@
+def Shop(user):
+    sql = f"SELECT CO2_BUDGET FROM GAME WHERE ID = {user}"
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    if not result:
+        print("Player not found.")
+        return
+
+    current_balance = result[0]
+    print(f"Your current balance: {current_balance}")
+    items = {
+        "Speedbooster_5min": 50,
+        "Protectingshield_5min": 50,
+        "Advanced_engines": 200,
+        "Advanced_fuselage": 200,
+    }
+
+    print("Available items:")
+    for item, price in items.items():
+        print(f"{item}: {price} currency")
+    selected_item = input("Enter the item you want to purchase: ")
+    if selected_item not in items:
+        print("Invalid item selection.")
+        return
+
+    item_price = items[selected_item]
+    if current_balance < item_price:
+        print("You don't have enough money.")
+        return
+    new_balance = current_balance - item_price
+    sql = f"UPDATE GAME SET CO2_BUDGET = {new_balance} WHERE USER = {user}"
+    cursor.execute(sql)
+    connection.commit()
+    print(f"Purchase of {selected_item} successful. Your new balance: {new_balance}")
+    return True
