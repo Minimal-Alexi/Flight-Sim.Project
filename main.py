4b8f1caddf6ce8229e63c5aecac56fea7d6a3bec
Alex <alexpop@metropolia.fi>
2023-10-02 10:16:46 +0300

    Added the user login and registry.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import mysql.connector
 import pygame
 from Database import (get_continent, get_continent_list, get_airport_list,
-                      get_airport_type_list, get_country_list, get_user_location, set_user_location, db_query)
+                      get_airport_type_list, get_country_list, get_user_location, set_user_location, db_query, update_player)
 
 connection = mysql.connector.connect(
          host='127.0.0.1',
          port= 3306,
          database='flight_game',
-         user='root',
-         password='PmmcMAsmni2004',
+         user='flight_sim',
+         password='menudb',
          autocommit=True
          )
 
@@ -71,31 +71,58 @@ def InternationalAirportFetcher(cursor, user_id=1):
 
 # After the algorithm fetches the airports, the player is asked if they want to select to go to one of the airports.
 # If the player says no they get returned to the main menu, otherwise the flight game starts
-def FlightGame():
-    return True
-
-def UserReg():
-    return True
 def UserReg(input):
     sql = f"SELECT max(id) FROM GAME"
     result = db_query(sql,cursor)
-    if cursor.rowcount == 1:
+    if cursor.rowcount == 1 and result[0]!=(None,):
         (maxi, ) = result[0]
-    maxi = int(maxi)
-    maxi = maxi + 1
-    sql = f"INSERT INTO GAME (ID,CO2_CONSUMED,CO2_BUDGET,LOCATION,SCREEN_NAME) VALUES ({maxi},0,10000,'EGCC','{input}')"
+        maxi = int(maxi)
+        maxi = maxi + 1
+    else:
+        maxi = 1
+    sql = f"INSERT INTO GAME (ID,MONEY,CO2_BUDGET,LOCATION,SCREEN_NAME,FUEL,FUEL_EFFICIENCY) VALUES ({maxi},100,10000,'EGCC','{input}',100,5)"
     db_query(sql,cursor)
 def UserLog(user, input):
-    sql = f"SELECT CO2_BUDGET,LOCATION,SCREEN_NAME,ID FROM GAME WHERE SCREEN_NAME = '{input}'"
+    sql = f"SELECT CO2_BUDGET,LOCATION,SCREEN_NAME,ID,MONEY,FUEL,FUEL_EFFICIENCY FROM GAME WHERE SCREEN_NAME = '{input}'"
     result = db_query(sql,cursor)
     if cursor.rowcount>0:
         for row in result:
-            print(f"Hello, you are {row[2]}, at airport {row[1]}, with a CO2_budget of {row[0]}")
+            print(f"Hello, you are {row[2]}, at airport {row[1]}, with a CO2_budget of {row[0]}. You have {row[4]}$, very rich :3.")
     user.location = row[1]
     user.CO2_Budget = row[0]
     user.databaseID = row[3]
+    user.Fuel = row[5]
+    user.Money= row[4]
+    user.Fuel_Efficiency = row[6]
+def Shop(user):
 
+    print(f"Your current balance: {user.Money}$")
+    print(f"Your current CO2 budget: {user.CO2_Budget}")
+    items = {
+        "Speedbooster_5min": 50,
+        "Protectingshield_5min": 50,
+        "Advanced_engines": 200,
+        "Advanced_fuselage": 200,
+    }
 
+    print("Available items:")
+    for item, price in items.items():
+        print(f"{item}: {price} currency")
+    selected_item = input("Enter the item you want to purchase: ")
+    if selected_item not in items:
+        print("Invalid item selection.")
+        return
+
+    item_price = items[selected_item]
+    if user.Money < item_price:
+        print("You don't have enough money.")
+        return
+    user.Money = user.Money - item_price
+    print(f"Purchase of {selected_item} successful. Your new balance: {user.Money}")
+    update_player(cursor,user)
+    return True
+def EventSystem():
+    return True
 class Player:
     databaseID = 0
     location = "Placeholder"
@@ -127,7 +154,8 @@ while run == True:
     print("2 - Move to an international airport: ")
     print("3 - Pick up quests from the airport: ")
     print("4 - Airport shop: ")
-    print("5 - Log out. ")
+    print("5 - Airplane stats: ")
+    print("6 - Log out. ")
     UsInput = int(input("Which choice would you like to pick?"))
     if UsInput == 1:
         print("WIP")
@@ -136,6 +164,8 @@ while run == True:
     elif UsInput == 3:
         print("WIP")
     elif UsInput == 4:
+        Shop(user)
+    elif UsInput == 5:
         print("WIP")
     else:
         run = False
