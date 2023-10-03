d10a3ec7542b3c2c7d43d4d4a12fe58b64f8c16d
Minimal-Alexi <142995783+Minimal-Alexi@users.noreply.github.com>
2023-10-03 18:48:57 +0300

    Implemented the shop (most of it), added random generated good-bye messages, made it so Ashifas international fetcher affects the player currently logged in.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from random import randint
 import pygame
 from Database import (get_continent, get_continent_list, get_airport_list,
                       get_airport_type_list, get_country_list, get_user_location, set_user_location, db_query, update_player)
@@ -31,8 +32,9 @@ def display_continent_list(continents):
         print(f"{counter}. {get_continent(x[0])}")
         counter += 1
 
-
-def InternationalAirportFetcher(cursor, user_id=1):
+# After the algorithm fetches the airports, the player is asked if they want to select to go to one of the airports.
+# If the player says no they get returned to the main menu, otherwise the flight game starts
+def InternationalAirportFetcher(cursor, user_id):
     # This function will run a cli menu where the user selects an international airport
     location = get_user_location(user_id, cursor)
     print(f"Current location: {location[1]}")
@@ -68,9 +70,7 @@ def InternationalAirportFetcher(cursor, user_id=1):
     set_user_location(airports[selection - 1][1], user_id, cursor)
 
     print(f"\nLocation updated to {airport_sel}")
-
-# After the algorithm fetches the airports, the player is asked if they want to select to go to one of the airports.
-# If the player says no they get returned to the main menu, otherwise the flight game starts
+#This function registers the user into the database
 def UserReg(input):
     sql = f"SELECT max(id) FROM GAME"
     result = db_query(sql,cursor)
@@ -82,6 +82,7 @@ def UserReg(input):
         maxi = 1
     sql = f"INSERT INTO GAME (ID,MONEY,CO2_BUDGET,LOCATION,SCREEN_NAME,FUEL,FUEL_EFFICIENCY) VALUES ({maxi},100,10000,'EGCC','{input}',100,5)"
     db_query(sql,cursor)
+#This function logs in hte players data into the python function, so we don't have to constantly call from the DB.
 def UserLog(user, input):
     sql = f"SELECT CO2_BUDGET,LOCATION,SCREEN_NAME,ID,MONEY,FUEL,FUEL_EFFICIENCY FROM GAME WHERE SCREEN_NAME = '{input}'"
     result = db_query(sql,cursor)
@@ -98,31 +99,51 @@ def Shop(user):
 
     print(f"Your current balance: {user.Money}$")
     print(f"Your current CO2 budget: {user.CO2_Budget}")
+    print(f"Your current CO2 budget: {user.Fuel}")
+    Refuel = 100 - user.Fuel
+    #All items have a money and CO2 price
     items = {
-        "Speedbooster_5min": 50,
-        "Protectingshield_5min": 50,
-        "Advanced_engines": 200,
-        "Advanced_fuselage": 200,
+        "+5km/l Fuel Efficiency": (1000,100),
+        "Refueling Services": (Refuel*10,Refuel*10+500),
+        "Environmental-Friendly Refueling Services": (Refuel * 10 + 500, Refuel * 10),
+        "One-Time Extra Cargo capacity": (200, 250),
+        "One-Time Fuel Drop Tanks": (200, 1000)
     }
 
     print("Available items:")
     for item, price in items.items():
-        print(f"{item}: {price} currency")
+        #Here we unpack the touples into two different variables.
+        (moneyprice, CO2_price) = price
+        print(f"{item}: {moneyprice}$, {CO2_price} CO2 tokens")
     selected_item = input("Enter the item you want to purchase: ")
     if selected_item not in items:
         print("Invalid item selection.")
-        return
-
-    item_price = items[selected_item]
-    if user.Money < item_price:
+        return False
+    #We do that again.
+    (moneyprice, CO2_price) = items[selected_item]
+    if user.Money < moneyprice:
         print("You don't have enough money.")
-        return
-    user.Money = user.Money - item_price
+        return False
+    if user.CO2_Budget < CO2_price:
+        print("Are you sure you want to go into negative CO2 budget?")
+        UsInput = input("Yes/No ")
+        if UsInput == "No":
+            return False
+    user.Money = user.Money - moneyprice
+    user.CO2_Budget = user.CO2_Budget - CO2_price
     print(f"Purchase of {selected_item} successful. Your new balance: {user.Money}")
     update_player(cursor,user)
     return True
-def EventSystem():
-    return True
+def Goodbye():
+    Randomgoodbye = randint(1, 5)
+    GoodbyeMessage = {
+            1:"Guess this was too Boe-ing for you",
+            2:"I'll just sit over here and watch, carry-on.",
+            3:"At this moment I think your head is in the clouds.",
+            4:"I'd stick around but I gotta jet.",
+            5:"Nooo...but think of all the Californians, they were looking UP to you...well...nevermind, it's probably better if you log out."
+        }
+    print(GoodbyeMessage[Randomgoodbye])
 class Player:
     databaseID = 0
     location = "Placeholder"
@@ -149,24 +170,26 @@ else:
     run = False
 while run == True:
     #The program will have to remind the player what airport they are located in:
-    #Or if they crashed
     print("1 - Move to a local airport: ")
     print("2 - Move to an international airport: ")
     print("3 - Pick up quests from the airport: ")
     print("4 - Airport shop: ")
-    print("5 - Airplane stats: ")
+    print("5 - Player stats: ")
     print("6 - Log out. ")
     UsInput = int(input("Which choice would you like to pick?"))
     if UsInput == 1:
         print("WIP")
     elif UsInput == 2:
-        InternationalAirportFetcher(cursor)
+        InternationalAirportFetcher(cursor,user.databaseID)
     elif UsInput == 3:
         print("WIP")
     elif UsInput == 4:
-        Shop(user)
+        if Shop(user) == True:
+            print("Purchase successful")
+        else:
+            print("Purchase failed")
     elif UsInput == 5:
         print("WIP")
     else:
         run = False
-print("Bye bye user!")
+Goodbye()
