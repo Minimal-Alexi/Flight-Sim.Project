61f3f29a172e9f905911789ebe4576d97f08b807
bytecodeble <yyilin@yahoo.com>
2023-10-05 11:33:02 +0300

    Saving purchased items and define items effects

--- Shop.py
+++ Shop.py
@@ -11,37 +11,63 @@ connection = mysql.connector.connect(
 # Create a cursor for all interactions with the MariaDB Database
 cursor = connection.cursor()
 def shop(user):
-    sql = f"SELECT CO2_BUDGET FROM GAME WHERE ID = {user}"
-    cursor.execute(sql)
-    result = cursor.fetchone()
-    if not result:
-        print("Player not found.")
-        return
-
-    current_balance = result[0]
-    print(f"Your current balance: {current_balance}")
+    print(f"Your current balance: {user.Money}$")
+    print(f"Your current CO2 budget: {user.CO2_Budget}")
+    print(f"Your current CO2 budget: {user.Fuel}")
+    Refuel = 100 - user.Fuel
     items = {
-        "Speedbooster_5min": 50,
-        "Protectingshield_5min": 50,
-        "Advanced_engines": 200,
-        "Advanced_fuselage": 200,
+        "+5km/l Fuel Efficiency": (1000, 100, 5),
+        "Refueling Services": (Refuel * 10, Refuel * 10 + 500),
+        "Environmental-Friendly Refueling Services": (Refuel * 10 + 500, Refuel * 10),
+        "One-Time Extra Cargo capacity": (200, 250),
+        "One-Time Fuel Drop Tanks": (200, 1000)
     }
 
-    print("Available items:")
-    for item, price in items.items():
-        print(f"{item}: {price} currency")
-    selected_item = input("Enter the item you want to purchase: ")
-    if selected_item not in items:
-        print("Invalid item selection.")
-        return
+    user.purchased_items = {}
 
-    item_price = items[selected_item]
-    if current_balance < item_price:
-        print("You don't have enough money.")
-        return
-    new_balance = current_balance - item_price
-    sql = f"UPDATE GAME SET CO2_BUDGET = {new_balance} WHERE USER = {user}"
-    cursor.execute(sql)
-    connection.commit()
-    print(f"Purchase of {selected_item} successful. Your new balance: {new_balance}")
-    return True
+    while True:
+        print("Available items:")
+        for item, price in items.items():
+            (moneyprice, CO2_price) = price
+            print(f"{item}: {moneyprice}$, {CO2_price} CO2 tokens")
+
+        print("Your purchased items:")
+        for item, quantity in user.purchased_items.items():
+            print(f"{item}: {quantity}")
+
+        selected_item = input("Enter the item you want to purchase or type 'show' to see your purchased items: ")
+
+        if selected_item == 'show':
+            continue
+
+        if selected_item not in items:
+            print("Invalid item selection.")
+            return False
+
+        (moneyprice, CO2_price, fuel_efficiency_increase) = items[selected_item]
+
+        if user.Money < moneyprice:
+            print("You don't have enough money.")
+            return False
+
+        if user.CO2_Budget < CO2_price:
+            print("Are you sure you want to go into a negative CO2 budget?")
+            UsInput = input("Yes/No ")
+            if UsInput == "No":
+                return False
+
+        user.Money = user.Money - moneyprice
+        user.CO2_Budget = user.CO2_Budget - CO2_price
+        print(f"Purchase of {selected_item} successful. Your new balance: {user.Money}")
+
+        # Apply the item's effect
+        user.Fuel_Efficiency += fuel_efficiency_increase
+
+        # Update purchased items dictionary
+        if selected_item in user.purchased_items:
+            user.purchased_items[selected_item] += 1
+        else:
+            user.purchased_items[selected_item] = 1
+
+        update_player(cursor, user)
+        return True
