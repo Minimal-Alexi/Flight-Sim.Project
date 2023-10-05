96bae20a1cc1f6c9d2ca1b7156592e0b6a3486d9
bytecodeble <yyilin@yahoo.com>
2023-10-05 11:41:42 +0300

    Saving purchased items and define items effects

--- Shop.py
+++ Shop.py
@@ -19,32 +19,37 @@ def shop(user):
         "+5km/l Fuel Efficiency": (1000, 100, 5),
         "Refueling Services": (Refuel * 10, Refuel * 10 + 500),
         "Environmental-Friendly Refueling Services": (Refuel * 10 + 500, Refuel * 10),
-        "One-Time Extra Cargo capacity": (200, 250),
-        "One-Time Fuel Drop Tanks": (200, 1000)
+        "One-Time Extra Cargo capacity": (200, 250,0),
+        "One-Time Fuel Drop Tanks": (200, 1000,0)
     }
 
     user.purchased_items = {}
+    selected_item = input("Enter the item you want to purchase or type 'show' to see your purchased items: ")
+
+    if selected_item == 'show':
+        continue
+
+    if selected_item not in items:
+        print("Invalid item selection.")
+        return False
+
+    (moneyprice, CO2_price, item_quantity) = items[selected_item]
+
+    if item_quantity <= 0:
+        print(f"No more {selected_item} available.")
+        continue
 
     while True:
         print("Available items:")
         for item, price in items.items():
             (moneyprice, CO2_price) = price
             print(f"{item}: {moneyprice}$, {CO2_price} CO2 tokens")
+            if item_quantity > 0:
+                print(f"{item}: {moneyprice}$, {item_quantity} Fuel Efficiency, {item_quantity} available")
 
         print("Your purchased items:")
         for item, quantity in user.purchased_items.items():
             print(f"{item}: {quantity}")
-
-        selected_item = input("Enter the item you want to purchase or type 'show' to see your purchased items: ")
-
-        if selected_item == 'show':
-            continue
-
-        if selected_item not in items:
-            print("Invalid item selection.")
-            return False
-
-        (moneyprice, CO2_price, fuel_efficiency_increase) = items[selected_item]
 
         if user.Money < moneyprice:
             print("You don't have enough money.")
@@ -60,14 +65,16 @@ def shop(user):
         user.CO2_Budget = user.CO2_Budget - CO2_price
         print(f"Purchase of {selected_item} successful. Your new balance: {user.Money}")
 
-        # Apply the item's effect
-        user.Fuel_Efficiency += fuel_efficiency_increase
+        # Apply the items' effects
+        user.Item_Effect += item_quantity
 
         # Update purchased items dictionary
         if selected_item in user.purchased_items:
             user.purchased_items[selected_item] += 1
         else:
             user.purchased_items[selected_item] = 1
+
+        items[selected_item] = (moneyprice, CO2_price, item_quantity - 1)
 
         update_player(cursor, user)
         return True
