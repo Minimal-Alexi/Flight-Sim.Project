e3e34296a2957989ababba9c49603629e441ee1f
bytecode <143608099+bytecodeble@users.noreply.github.com>
2023-11-27 14:24:25 +0200

    main html page by yilin

---
+++ game-HTML/js/main.js
@@ -0,0 +1,33 @@
+'use strict';
+/* 1. show map using Leaflet library. (L comes from the Leaflet library) */
+/*
+const map = L.map('map', {tap: false});
+L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+  maxZoom: 20,
+  subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+}).addTo(map);
+map.setView([60, 24], 7);
+ */
+
+// global variables
+
+// icons
+
+// form for player name
+
+// function to fetch data from API
+
+// function to update game status
+
+// function to show weather at selected airport
+
+// function to check if any goals have been reached
+
+// function to update goal data and goal table in UI
+
+// function to check if game is over
+
+// function to set up game
+// this is the main function that creates the game and calls the other functions
+
+// event listener to hide goal splash
