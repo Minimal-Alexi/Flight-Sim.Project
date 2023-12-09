375b96c5dd3fc9cba6e45e546f9ed62775d04847
Alex <alexpop@metropolia.fi>
2023-12-09 14:06:55 +0200

    Continent selector has already been implemented in the main page.

--- static/Continent Selector Templates/js/continent.js
+++
@@ -1,7 +0,0 @@
-function submitForm() {
-    var selectedContinent = document.getElementById("continent").value;
-    alert("You selected: " + selectedContinent);
-}
-function goBack() {
-  window.location.href = 'Main Page.html';
-}
